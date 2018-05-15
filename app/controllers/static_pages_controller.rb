7e5ebeec4acf38a59d418774e114227e78636851
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-05-15 13:20:06 +0700

    finish chapter 3

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'application#hello'
+  root 'static_pages#home'
+
+  get  'static_pages/about'
+
+  get  'static_pages/contact'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+
+
+
 
 end
