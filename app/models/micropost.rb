1922fabd4325ac0fbe0dcbfaa344d30808aee23d
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-05-24 16:29:44 +0700

    Finish chapter14

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,14 +5,19 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: {maximum: Settings.micropost.content.max_length}
   validate  :picture_size
 
-  scope :order_by_created_at, ->{order(created_at: :asc)}
-
   mount_uploader :picture, PictureUploader
+
+  scope :order_by_created_at, ->{order created_at: :desc}
+  scope :by_followed, lambda do |user_id|
+    where("user_id IN (SELECT followed_id FROM relationships
+      WHERE  follower_id = :user_id)
+      OR user_id = :user_id", user_id: user_id).order created_at: :desc
+  end
 
   private
 
   def picture_size
-    if picture.size >Settings.micropost.picture.size.megabytes
+    if picture.size > Settings.micropost.picture.size.megabytes
       errors.add :picture, t("model.mocropost.error")
     end
   end
